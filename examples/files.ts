057bfa95f5d33c0477b400bd66177c1aae3c3f8b
Joshua Kidd <hey@g33kidd.dev>
2023-09-05 05:27:48 -0600

    Updated example with new scaffold

--- examples/files.ts
+++ examples/files.ts
@@ -1,13 +1,14 @@
-import { Engine, action, pipe, staticPipe } from "bamboo";
+import { engine, action, pipe, staticPipe } from "bamboo";
 
 // Define an action with a Definition, Handler, and (optionally) pipes.
 const helloWorld = action("GET /", (endpoint) => {
   return endpoint.file("./static/index.html");
 });
 
-// Create the engine and start the server.
-export default new Engine({
-  // Adds the ability to serve static assets.
-  pipes: [staticPipe],
+// Configure the engine with our action.
+engine.configure({
   actions: [helloWorld],
-}).server();
+});
+
+// Start the engine.
+engine.serve();
